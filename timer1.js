ab599a73f4be112fffa8fb438cdc709c8978b473
JCON3DEV <jconheasman@gmail.com>
2020-07-29 04:02:28 +0000

    alarm timer with text works well, but needs to skip over the fail conditions. needs improving

---
+++ timer1.js
@@ -0,0 +1,30 @@
+const secondsArray = process.argv.slice(2);
+// let secondsArray = [3,5,9,10,15];
+
+const alarmTimer = function (secondsArray) {
+  for (let second of secondsArray) {
+    console.log(second);
+    if (typeof secondsArray[second] === "string" || secondsArray[second] < 0) {
+      console.log("type in positive numbers");
+      second++; // do not beleive this is skipping the alarm
+      //do not know why
+    }
+    setTimeout(() => {
+      process.stdout.write('BOOM');
+    }, (second *1000));
+  }
+};
+alarmTimer(secondsArray);
+
+// const timerFunct = setTimeout(() => {
+//   process.stdout.write('BOOM'); // this makes a timer sound  
+// }, 1000);
+// process.stdout.write('BOOM'); // this makes a timer sound
+/* Should beep at;
+
+    3 seconds
+    5 seconds
+    9 seconds
+    10 seconds
+    15 seconds
+*/
